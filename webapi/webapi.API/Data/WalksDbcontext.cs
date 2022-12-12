bcd91cb0b050d2427bc0c04b0668b24b6f81ab9a
RishabhHarman04 <er.23rishabh@gmail.com>
2022-12-12 17:11:06 +0530

    Created Database

---
+++ webapi/webapi.API/Data/WalksDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.API.Models.Domain;
+
+namespace webapi.API.Data
+{
+    public class WalksDbcontext: DbContext
+    {
+        public WalksDbcontext(DbContextOptions<WalksDbcontext> options): base(options) 
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+} 
--- webapi/webapi.API/Program.cs
+++ webapi/webapi.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using webapi.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WalksDbcontext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("Walks")); 
+});
 
 var app = builder.Build();
 
